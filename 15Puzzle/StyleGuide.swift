0d7554b92bf6a9ee20334da54aac2a1cc366f5c2
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2021-01-06 22:09:43 -0500

    refactor

---
+++ 15Puzzle/StyleGuide.swift
@@ -0,0 +1,28 @@
+//
+//  StyleGuide.swift
+//  15Puzzle
+//
+//  Created by Brandon Fong on 1/6/21.
+//
+
+import UIKit
+
+extension UIFont {
+  class var largeHeader: UIFont {
+    return UIFont(name: "AvenirNext-Bold", size: 30)! // force unwrap because I know these exist in xcode
+  }
+  
+  class var subtitle: UIFont {
+    UIFont(name: "AvenirNext-Regular", size: 14)!
+  }
+}
+
+extension UIColor {
+    class var boxColor: UIColor {
+        return UIColor(red: 32/255, green: 115/255, blue: 152/255, alpha: 1.0)
+    }
+    
+    class var appGray: UIColor {
+        return UIColor(red: 202/255, green: 213/255, blue: 226/255, alpha: 1.0)
+    }
+}
--- 15Puzzle/VC+UISetup.swift
+++ 15Puzzle/VC+UISetup.swift
@@ -140,23 +140,3 @@ extension ViewController {
         space.trailingAnchor.constraint(equalTo: grid.trailingAnchor, constant: -(totalGridMargin * 5 / 4 / 2)).isActive = true
     }
 }
-
-extension UIFont {
-  class var largeHeader: UIFont {
-    return UIFont(name: "AvenirNext-Bold", size: 30)! // force unwrap because I know these exist in xcode
-  }
-  
-  class var subtitle: UIFont {
-    UIFont(name: "AvenirNext-Regular", size: 14)!
-  }
-}
-
-extension UIColor {
-    class var boxColor: UIColor {
-        return UIColor(red: 32/255, green: 115/255, blue: 152/255, alpha: 1.0)
-    }
-    
-    class var appGray: UIColor {
-        return UIColor(red: 202/255, green: 213/255, blue: 226/255, alpha: 1.0)
-    }
-}
